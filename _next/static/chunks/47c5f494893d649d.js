64ad0462d9f126304e0498125a00d579ba6df80b
Cosmo19 <Cosmo19@users.noreply.github.com>
2025-10-15 10:19:11 +0000

    deploy: aa8a284f766d90a36dec1f907d032df3295db717

---
+++ _next/static/chunks/47c5f494893d649d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/529b37e1b0fce377.js",
+  "static/chunks/turbopack-85daae9449817ae2.js"
+])
---
+++ _next/static/chunks/f56b66a81eb16e7e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/529b37e1b0fce377.js",
+  "static/chunks/turbopack-949197834b57645a.js"
+])
