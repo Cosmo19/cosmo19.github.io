35523a2b0f76173b42fd0cd6eb99f2ea820ad696
Cosmo Elford <cosmoelford@Cosmos-MacBook-Pro.local>
2026-04-25 17:29:46 +0900

    changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/', // must match your GitHub repo name
   images: {
     unoptimized: true, // required for static export
   },
