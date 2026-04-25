af12fe7dbceca3eb4db8397de19c8313cde68404
Cosmo Elford <cosmoelford@Cosmos-MacBook-Pro.local>
2026-04-25 17:34:05 +0900

    redeployment

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/cosmo19', // must be exactly as it appears in GitHub
+  assetPrefix: '/cosmo19', // add this too
   images: {
-    unoptimized: true, // required for static export
+    unoptimized: true,
   },
 };
 
